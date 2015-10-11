0c83fc851a99081ec4dfd8ca937b749bc274ba53
HsHWB <Hswbinforgithub@gmail.com>
2015-10-11 18:05:50 +0800

    #activity主题淡入淡出效果完成

--- app/src/main/java/com/example/root/theme/MainActivity.java
+++ app/src/main/java/com/example/root/theme/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.root.theme;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mainButton = (Button)this.findViewById(R.id.main_button);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
